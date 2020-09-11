fd865c7773d11b488b72df5700fca88ce6b34943
Seggan <segganew@gmail.com>
2020-09-10 21:40:59 -0400

    Updated licence

--- src/main/java/io/github/seggan/sfcalc/CalcCompleter.java
+++ src/main/java/io/github/seggan/sfcalc/CalcCompleter.java
@@ -12,6 +12,22 @@ import java.util.Collections;
 import java.util.List;
 
 public class CalcCompleter implements TabCompleter {
+//    Copyright (C) 2020 Seggan
+//    Email: segganew@gmail.com
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//            (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
     private final List<String> commands = new ArrayList<>();
 
     @Override
