882efdaddc259f989374c6810182a2325ae9522d
Riley <69326411+Mooy1@users.noreply.github.com>
2021-06-17 11:49:50 -0500

    Update StringRegistry.java

--- src/main/java/io/github/seggan/sfcalc/StringRegistry.java
+++ src/main/java/io/github/seggan/sfcalc/StringRegistry.java
@@ -1,13 +1,13 @@
 package io.github.seggan.sfcalc;
 
-import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Pattern;
 
 import javax.annotation.Nonnull;
 
 import lombok.Getter;
 
 import org.apache.commons.lang.Validate;
-import org.bukkit.configuration.file.YamlConfiguration;
 
 import io.github.mooy1.infinitylib.configuration.AddonConfig;
 import me.mrCookieSlime.Slimefun.cscorelib2.chat.ChatColors;
@@ -15,6 +15,7 @@ import me.mrCookieSlime.Slimefun.cscorelib2.chat.ChatColors;
 @Getter
 public final class StringRegistry {
 
+    private final Pattern percentPattern = Pattern.compile("(%s|%d)");
     private final String headerString;
     private final String headerAmountString;
     private final String stackString;
@@ -28,22 +29,34 @@ public final class StringRegistry {
     private final String invalidNumberString;
 
     StringRegistry(AddonConfig config) {
-        if (config.getString("header-string").contains("%s")) {
-            // The config is outdated, overwrite it with default values
-            config.resetToDefaults();
-        }
-
-        headerString = config.getString("header-string");
-        headerAmountString = config.getString("header-amount-string");
-        stackString = config.getString("stack-string");
-        amountString = config.getString("amount-string");
-        neededString = config.getString("needed-string");
+        headerString = reformat(config, "header-string", 1);
+        headerAmountString = reformat(config, "header-amount-string", 2, 1);
+        stackString = reformat(config, "stack-string", 1, 2, 3, 4);
+        amountString = reformat(config, "amount-string", 2, 1);
+        neededString = reformat(config, "needed-string", 2, 1);
         noItemString = config.getString("no-item-string");
         notANumberString = config.getString("not-a-number-string");
         tooManyCategoriesString = config.getString("category-error-string");
         tooManyItemsString = config.getString("item-error-string");
         notAPlayerString = config.getString("not-a-player-string");
         invalidNumberString = config.getString("invalid-number-string");
+
+        config.save();
+    }
+
+    private String reformat(AddonConfig config, String key, int... numbers) {
+        String val = config.getString(key);
+
+        AtomicInteger i = new AtomicInteger();
+        String formatted = this.percentPattern.matcher(val)
+                .replaceAll(matchResult -> "%" + numbers[i.getAndIncrement()]);
+
+        if (i.get() == numbers.length) {
+            config.set(key, formatted);
+            return formatted;
+        }
+
+        return val;
     }
 
     @Nonnull
