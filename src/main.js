d0c9072f6d7d93186a7f80fe8cdfdba8a36b5629
Seggan <segganew@gmail.com>
2021-07-28 18:05:28 -0400

    Try this

--- src/main.js
+++ src/main.js
@@ -5,14 +5,12 @@ function getJSON(url) {
 }
 
 document.onload = function() {
-    document.getElementById('#calculator').onsubmit = function(e) {
+    document.getElementById('#calculator').onsubmit = function() {
         fetch('https://raw.githubusercontent.com/Seggan/SFCalc/gh-pages/src/items.json')
         .then(res => res.json())
         .then(items => {
             alert(items[0].name);
         })
         .catch(err => console.error(err));
-
-        return true;
     };
 };
