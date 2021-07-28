61181bde56d6140e8e314fd407732caf75c05cba
Seggan <segganew@gmail.com>
2021-07-28 17:47:32 -0400

    Try this

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 document.onload = function() {
     document.getElementById('#calculator').onsubmit = function() {
         fetch('https://raw.githubusercontent.com/Seggan/SFCalc/gh-pages/src/items.json')
-        .then(res => res.json())
-        .then(items => {
+        .then(res => res.json().then(items => {
             alert(items[0].name);
-        })
+        }))
         .catch(err => console.error);
 
         return false;
