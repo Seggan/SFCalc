2b59d2cc5b3fc88f3d963e9f98e8f26018da5d8e
Seggan <segganew@gmail.com>
2021-07-28 17:29:19 -0400

    Maybe...

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ $(function() {
         $.getJSON("https://raw.githubusercontent.com/Seggan/SFCalc/gh-pages/src/items.json", function(items) {
             alert(items[0].name);
         });
+        return false;
     });
 });
