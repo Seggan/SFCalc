e0c55f30bb4c9f04d1a0da6efd3685d568f5405f
Seggan <segganew@gmail.com>
2021-07-28 17:15:34 -0400

    Why...

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-$(function() { $('.calculator').on('submit', function() {
-    $.getJSON("https://raw.githubusercontent.com/Seggan/SFCalc/gh-pages/src/items.json", function(items) {
-        alert(items[0].id);
+$(function() { 
+    $('.calculator').onSubmit(function() {
+        $.getJSON("https://raw.githubusercontent.com/Seggan/SFCalc/gh-pages/src/items.json", function(items) {
+            alert(items[0].id);
+        });
     });
-});});
+});
