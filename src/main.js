de117f3f46f6c83860c5393dedf1408991c11911
Seggan <segganew@gmail.com>
2021-07-28 17:18:27 -0400

    pleeeeeeease

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 $(function() { 
     $('.calculator').onSubmit(function() {
         $.getJSON("https://raw.githubusercontent.com/Seggan/SFCalc/gh-pages/src/items.json", function(items) {
-            alert(items[0].id);
+            alert(items[0].name);
         });
     });
 });
