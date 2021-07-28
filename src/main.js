89a10034a1078415617a1e6d48e439b4757cea17
Seggan <segganew@gmail.com>
2021-07-28 16:58:56 -0400

    Form

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-$(function() {
-    $.getJSON("https://raw.githubusercontent.com/Seggan/SFCalc/gh-pages/src/items.json", function(data) {
-        alert(data[0].name);
+$('.calculator').on('submit', function() {
+    var items;
+    $.getJSON("https://gitcdn.link/repo/Seggan/SFCalc/gh-pages/src/items.json", function(data) {
+        items = data;
     });
+
+    document.write(items[0].id);
 });
