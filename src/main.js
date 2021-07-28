73688e459df96d749087a1291c89776d770cab1b
Seggan <segganew@gmail.com>
2021-07-28 17:59:43 -0400

    Seriously...

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
+function getJSON(url) {
+    var req = new XMLHttpRequest();
+    req.open();
+    req.send('GET', url, false);
+}
+
 document.onload = function() {
     document.getElementById('#calculator').onsubmit = function() {
         fetch('https://raw.githubusercontent.com/Seggan/SFCalc/gh-pages/src/items.json')
-        .then(res => res.json().then(items => {
+        .then(res => res.json())
+        .then(items => {
             alert(items[0].name);
-        }))
+        })
         .catch(err => console.error);
 
-        return false;
+        return true;
     };
 };
