b8f77bbfdc5fc261b82147e8d12d5557fd632033
Seggan <segganew@gmail.com>
2021-07-28 17:18:53 -0400

    oop

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 $(function() { 
-    $('.calculator').onSubmit(function() {
+    $('.calculator').submit(function() {
         $.getJSON("https://raw.githubusercontent.com/Seggan/SFCalc/gh-pages/src/items.json", function(items) {
             alert(items[0].name);
         });
