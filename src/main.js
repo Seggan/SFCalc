4df5c0291747a8c54ed912a5d669b7fdc9f9d9a6
Seggan <segganew@gmail.com>
2021-07-29 13:31:26 -0400

    Try again

--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
-var getJSON = function(url, callback) {
-    fetch(url)
-    .then(res => res.json())
-    .then(out => {
-        callback(out);
-    })
-    .catch(err => console.error);
-};
+var itemObj;
+fetch('https://raw.githubusercontent.com/Seggan/SFCalc/gh-pages/src/items.json')
+.then(res => res.json())
+.then(out => {
+    itemObj = out;
+})
+.catch(err => console.error);
 
 window.onload = e => {
     document.getElementById('calculator').onsubmit = e => {
-        getJSON('https://raw.githubusercontent.com/Seggan/SFCalc/gh-pages/src/items.json', items => {
-            alert(items[0].name);
-        });
+        alert(itemObj[0].id);
 
         return false;
     };
