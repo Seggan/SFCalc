09f515333b9ec80dbec57dadf9113732df817df2
Seggan <segganew@gmail.com>
2021-07-29 09:35:32 -0400

    More debug

--- src/main.js
+++ src/main.js
@@ -3,19 +3,19 @@ var getJSON = function(url, callback) {
     xhr.open('GET', url, true);
     xhr.responseType = 'json';
     xhr.onload = function() {
-      var status = xhr.status;
-      if (status === 200) {
-        callback(xhr.response);
-      } else {
-        console.error(status);
-      }
+        alert('code');
+        var status = xhr.status;
+        if (status === 200) {
+            callback(xhr.response);
+        } else {
+            console.error(status);
+        }
     };
     xhr.send();
 };
 
 window.onload = e => {
     document.getElementById('calculator').onsubmit = e => {
-        alert('code');
         getJSON('https://raw.githubusercontent.com/Seggan/SFCalc/gh-pages/src/items.json', items => {
             alert(items[0].name);
         });
