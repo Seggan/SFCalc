c0a3144ac478cc7e2d89c817e3b4e32a550aad6e
Seggan <segganew@gmail.com>
2021-07-29 22:15:20 -0400

    Try a diff way

--- src/main.js
+++ src/main.js
@@ -1,24 +1,20 @@
-var itemList;
-fetch('https://raw.githubusercontent.com/Seggan/SFCalc/gh-pages/src/items.json')
-.then(res => res.json())
-.then(out => itemList = out)
-.catch(err => console.error);
-
-var items = {};
-
-for (const key in itemList) {
-    console.log(key);
-    var item = itemList[key];
-    console.log(item);
-    items[item.id] = item;
-}
-
-console.log(items);
-
 window.onload = e => {
-    document.getElementById('calculator').onsubmit = e => {
-        alert(itemList[0].id);
-
-        return false;
-    };
+    fetch('https://raw.githubusercontent.com/Seggan/SFCalc/gh-pages/src/items.json')
+    .then(res => res.json())
+    .then(itemList => {
+        var items = {};
+        for (const key in itemList) {
+            console.log(key);
+            var item = itemList[key];
+            console.log(item);
+            items[item.id] = item;
+        }
+    
+        document.getElementById('calculator').onsubmit = e => {
+            alert(itemList[0].id);
+    
+            return false;
+        };
+    })
+    .catch(err => console.error);    
 };
