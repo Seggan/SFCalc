e52cac053f51e7007ca570fb64543d9dd761bcb4
Seggan <segganew@gmail.com>
2021-07-29 21:09:23 -0400

    Finally starting to get somewhere

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
-var itemObj;
+var itemList;
 fetch('https://raw.githubusercontent.com/Seggan/SFCalc/gh-pages/src/items.json')
 .then(res => res.json())
-.then(out => itemObj = out)
+.then(out => itemList = out)
 .catch(err => console.error);
+
+var items = {};
+
+for (const item of itemList) {
+    items[item.id] = item;
+}
+
+console.log(items["OSMIUM_DUST"]);
 
 window.onload = e => {
     document.getElementById('calculator').onsubmit = e => {
-        alert(itemObj[0].id);
+        alert(itemList[0].id);
 
         return false;
     };
