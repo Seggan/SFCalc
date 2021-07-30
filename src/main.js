ad9745abc5444bcf9ced20bd9e9aec4dde70f5ae
Seggan <segganew@gmail.com>
2021-07-30 16:41:30 -0400

    Load on start instead

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
+var items = {};
+
+fetch('https://raw.githubusercontent.com/Seggan/SFCalc/gh-pages/src/items.json')
+.then(res => res.json())
+.then(itemList => {
+    for (const key in itemList) {
+        var item = itemList[key];
+        items[item.id] = item;
+    }
+}).catch(err => console.error);  
+
 window.onload = e => {
-    fetch('https://raw.githubusercontent.com/Seggan/SFCalc/gh-pages/src/items.json')
-    .then(res => res.json())
-    .then(itemList => {
-        var items = {};
-        for (const key in itemList) {
-            var item = itemList[key];
-            items[item.id] = item;
-        }
-    
-        document.getElementById('calculator').onsubmit = e => {
-            alert(itemList[0].id);
-    
-            return false;
-        };
-    })
-    .catch(err => console.error);    
+    document.getElementById('calculator').onsubmit = e => {
+        alert(itemList[0].id);
+
+        return false;
+    };
 };
