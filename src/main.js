c3a2ff187ac22c71ea2f6a7afd227c52e1dbfc46
Seggan <segganew@gmail.com>
2021-08-10 22:11:33 -0400

    Primitive calc?

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 var items = {};
+const blacklistedItems = [
+    "UU_MATTER",
+    "SILICON"
+];
+const blacklistedRecipes = [
+    "ore_washer",
+    "geo_miner",
+    "gold_pan",
+    "mob_drop",
+    "barter_drop",
+    "ore_crusher"
+];
 
 fetch('https://raw.githubusercontent.com/Seggan/SFCalc/gh-pages/src/items.json')
 .then(res => res.json())
@@ -8,13 +20,38 @@ fetch('https://raw.githubusercontent.com/Seggan/SFCalc/gh-pages/src/items.json')
     }
 }).catch(err => console.error);
 
-function calculate(state, item) {
+function calculate(item) {
+    var results = {};
+    for (const ing of item.recipe) {
+        var value = ing.value;
+        if (ing.slimefun) {
+            var ret = calculate(value);
+            for (const key in ret) {
+                if (key in results) {
+                    var inThere = results[key];
+                    inThere += ret[key];
+                    results[key] = inThere;
+                } else {
+                    results[key] = ret[key];
+                }
+            }
+        } else {
+            if (value in results) {
+                var inThere = results[value];
+                inThere++;
+                results[value] = inThere;
+            } else {
+                results[value] = 1;
+            }
+        }
+    }
 
+    return results;
 }
 
 window.onload = e => {
     document.getElementById('calculator').onsubmit = e => {
-        alert(items['OSMIUM_DUST'].id);
+        alert(calculate(document.getElementById('id').value));
 
         return false;
     };
